8c12e1c67fcf899c9f225f68add9ad1e55f2e55b
Daniel Imms <daimms@microsoft.com>
2016-09-15 17:26:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vscode-terminal-here
+
+Exposes the command `terminalHere.create` that creates a terminal at the directory of the current file. This can be accessed through the command palette (<kbd>ctrl</kbd>/<kbd>cmd</kbd>+<kbd>p</kbd>) or by attaching a [custom keybinding](https://code.visualstudio.com/docs/customization/keybindings) to it.
---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import path = require('path');
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('terminalHere.create', () => {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        
+        let document = editor.document;
+        if (!document) {
+            return;
+        }
+
+        let uri = document.uri;
+        if (!uri) {
+            return;
+        }
+
+        let dir = path.dirname(uri.fsPath);
+        let terminal = vscode.window.createTerminal();
+        terminal.show(false);
+        terminal.sendText(`cd ${dir}`);
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
