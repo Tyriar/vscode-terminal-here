1aba1a66e26068f5baa8fb7506a5f71d279dc3b0
Daniel Imms <daimms@microsoft.com>
2019-08-12 11:17:28 -0700

    Add deprecation notice and message box

--- src/extension.ts
+++ src/extension.ts
@@ -27,6 +27,14 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+    
+    const items: string[] = [ 'More info' ];
+    vscode.window.showInformationMessage('The Terminal Here extension has been deprecated, you can now do this using a VS Code setting', ...items).then(value => {
+        if (!value) {
+            return;
+        }
+        vscode.env.openExternal(vscode.Uri.parse('https://github.com/Tyriar/vscode-terminal-here/issues/20'));
+    });
 }
 
 export function deactivate() {
